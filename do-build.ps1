1d844e4b1ed86dad757e69155adac7d09a12096f
plastikfan <plastikfan@outlook.com>
2020-08-09 21:41:07 +0100

    revert previous non-fix

--- do-build.ps1
+++ do-build.ps1
@@ -1,8 +1,3 @@
 
 Set-Location .\Elizium.FakeBuddy
-
-$PairsToWrite = @(@("Artist", "Plastikman"), @("Song", "Marbles"))
-$krayolaTheme = Get-KrayolaTheme;
- Write-ThemedPairsInColour -Pairs $PairsToWrite -Theme $krayolaTheme
-
 Invoke-Build build
