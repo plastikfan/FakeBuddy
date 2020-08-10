14efa354ec6ce08182cd6afcfae58abf969edaa9
plastikfan <plastikfan@outlook.com>
2020-08-10 17:34:18 +0100

    write in colour as a test

--- show-pester-version.ps1
+++ show-pester-version.ps1
@@ -1,7 +1,12 @@
 
-$PSVersionTable
 
-Write-Host "====================================="
-$pm = get-module -Name PSScriptAnalyzer
-Write-Host ">>> Showing PSScriptAnalyzer Version $($pm.version.ToString()) >>>"
+# Write-Host "====================================="
+# $pm = get-module -Name PSScriptAnalyzer
+# Write-Host ">>> Showing PSScriptAnalyzer Version $($pm.version.ToString()) >>>"
 
+$theme = Get-KrayolaTheme
+$line = @(
+  @('Message', 'i am stumped'),
+  @('Complaint', 'give us a bleeding break')
+)
+Write-ThemedPairsInColour -Pairs $line -Theme $theme
