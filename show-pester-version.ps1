ecc4510204add56075dbd422bade63b1237b79b2
plastikfan <plastikfan@outlook.com>
2020-08-10 16:52:24 +0100

    blah

--- show-pester-version.ps1
+++ show-pester-version.ps1
@@ -1,4 +1,4 @@
 
-$version = get-module -Name Pester
-Write-Host ">>> Showing Pester Version $($version.version.ToString()) >>>"
+$pm = get-module -Name Pester
+Write-Host ">>> Showing Pester Version $($pm.version.ToString()) >>>"
 
