3782cc8ffb3cc348fb70b7532ac081b600697992
plastikfan <plastikfan@outlook.com>
2020-08-12 11:24:00 +0100

    Add paths to check module built

--- check-module-built.ps1
+++ check-module-built.ps1
@@ -4,9 +4,9 @@ Push-Location .\Elizium.FakeBuddy
 $folderExists = Test-Path -Path .\Output\Elizium.FakeBuddy
 $defintionExists = Test-Path -Path .\Output\Elizium.FakeBuddy\Elizium.FakeBuddy.psd1
 
-Write-Host "MODULE FOLDER EXISTS: $folderExists";
-Write-Host "MODULE DEFINITION EXISTS: $defintionExists";
+Write-Host "MODULE FOLDER EXISTS: $folderExists (path: $(Convert-Path -Path .\Output\Elizium.FakeBuddy))";
+Write-Host "MODULE DEFINITION EXISTS: $defintionExists (path $(Convert-Path -Path .\Output\Elizium.FakeBuddy\Elizium.FakeBuddy.psd1))";
 
  $env:PSModulePath -split ';'
- 
+
 Pop-Location
